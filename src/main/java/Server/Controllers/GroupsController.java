20a485ac3d63d78718aa866261aa42488301922d
Martin Stefanov <146194351+MartinStefanov16@users.noreply.github.com>
2024-10-25 15:50:31 +0300

    code cleanup

--- src/main/java/Server/Controllers/GroupsController.java
+++ src/main/java/Server/Controllers/GroupsController.java
@@ -1,16 +1,11 @@
 package Server.Controllers;
 
-import Server.Model.Entities.Groups;
 import Server.Services.GroupsService;
-import Server.Services.Repository.GroupsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class GroupsController {
